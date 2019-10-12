26e1eea5ac8b90121a534476587a78a709949d0b
zjialin <zjialin@outlook.com>
2019-10-12 15:15:45 +0800

    初始化工程

--- base-msg-center-web/src/main/java/com/pingan/city/msg/web/ApplicationMain.java
+++ base-msg-center-web/src/main/java/com/pingan/city/msg/web/ApplicationMain.java
@@ -1,5 +1,6 @@
 package com.pingan.city.msg.web;
 
+import org.apache.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,12 +8,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-//@EnableServiceComb
+@EnableServiceComb
 @ComponentScan(basePackages = {"com.pingan.city.msg"})
 @MapperScan(basePackages = {"com.pingan.city.msg.dao.mapper"})
 @EnableTransactionManagement
 public class ApplicationMain {
-
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "dev");
         SpringApplication.run(ApplicationMain.class, args);
